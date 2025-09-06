880214cc8e1c7f58684448925ea369ac7bc33157
shebasatan <satansheba@gmail.com>
2025-09-06 15:45:54 -0700

    Create protection4.1.1.h

---
+++ protection4.1.1.h
@@ -0,0 +1,85 @@
+≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈                  
+                   Welcome to termux 
+                                     
+                         htmlgold    
+                                     
+              Sat Sep  6 15:32:32 PDT
+ 2025                                
+                      Good afternoon 
+                                     
+≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈                  
+~ >>gem install footed lion
+ERROR:  Could not find a valid gem 'f
+ooted' (>= 0) in any repository
+ERROR:  Possible alternatives: rooted
+, tooled, sorted, rootee, opted, note
+d, hooked, forte, formed, forked     
+Fetching rack-3.2.1.gem
+Fetching rack-protection-4.1.1.gem
+Fetching tilt-2.6.1.gem
+Fetching mustermann-3.0.4.gem
+Fetching lion-0.0.5.gem
+Fetching rack-session-2.1.1.gem
+Fetching sinatra-4.1.1.gem
+Successfully installed tilt-2.6.1
+Successfully installed rack-3.2.1
+Successfully installed rack-session-2
+.1.1
+Successfully installed rack-protectio
+n-4.1.1
+Successfully installed mustermann-3.0
+.4
+Successfully installed sinatra-4.1.1
+Successfully installed lion-0.0.5
+Parsing documentation for tilt-2.6.1
+Installing ri documentation for tilt-
+2.6.1
+Parsing documentation for rack-3.2.1
+Installing ri documentation for rack-
+3.2.1
+Parsing documentation for rack-sessio
+n-2.1.1
+Installing ri documentation for rack-
+session-2.1.1
+Parsing documentation for rack-protec
+tion-4.1.1
+Installing ri documentation for rack-
+protection-4.1.1
+Parsing documentation for mustermann-
+3.0.4
+Installing ri documentation for muste
+rmann-3.0.4
+Parsing documentation for sinatra-4.1
+.1
+Installing ri documentation for sinat
+ra-4.1.1
+Parsing documentation for lion-0.0.5
+Installing ri documentation for lion-
+0.0.5
+Done installing documentation for til
+t, rack, rack-session, rack-protectio
+n, mustermann, sinatra, lion after 71
+ seconds
+7 gems installed
+~ >>gem install footed team
+ERROR:  Could not find a valid gem 'f
+ooted' (>= 0) in any repository
+ERROR:  Possible alternatives: rooted
+, tooled, sorted, rootee, opted, note
+d, hooked, forte, formed, forked
+ERROR:  Could not find a valid gem 't
+eam' (>= 0) in any repository
+ERROR:  Possible alternatives: beam, 
+seam, steam, tam, tea, teams, term
+~ >>gem install teams
+Fetching teams-0.1.0.gem
+Successfully installed teams-0.1.0
+Parsing documentation for teams-0.1.0
+Installing ri documentation for teams
+-0.1.0
+Done installing documentation for tea
+ms after 1 seconds
+1 gem installed
+~ >>
